7291a4f85712a5b2fb73b27738161eeb05b53975
2573874409@qq.com <z18344203426@qq.com>
2023-01-10 11:07:06 +0800

    更新 flashdns.sh

--- flashdns.sh
+++ flashdns.sh
@@ -1,5 +1,5 @@
+# 此脚本用于刷新 github-action 中的dns缓存。
 cat '/etc/resolv.conf'
 sudo systemctl restart systemd-resolved
 sudo resolvectl statistics
 sudo resolvectl flush-caches
-# 此脚本用于刷新 github-action 中的dns缓存。
