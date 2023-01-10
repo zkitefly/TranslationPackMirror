73f920d3c7f8e5a2cdbbc14d7949064584143f2e
2573874409@qq.com <z18344203426@qq.com>
2023-01-10 11:46:14 +0800

    调试:尝试添加dns以解决问题
    希望非常的渺茫

--- flashdns.sh
+++ flashdns.sh
@@ -1,5 +1,14 @@
 # 此脚本用于刷新 github-action 中的dns缓存。
+echo '1——————————'
 cat '/etc/resolv.conf'
+echo '2——————————'
+echo "nameserver 114.114.114.114
+nameserver 8.8.8.8">'/etc/resolv.conf'
+echo '3——————————'
+cat '/etc/resolv.conf'
+echo '4——————————'
 sudo systemctl restart systemd-resolved
+echo '5——————————'
 sudo resolvectl statistics
+echo '6——————————'
 sudo resolvectl flush-caches
