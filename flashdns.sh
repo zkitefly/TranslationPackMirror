6a5cc84d1ba03fa1bf118e3ffde2b999b4c48a52
2573874409@qq.com <z18344203426@qq.com>
2023-01-10 12:12:13 +0800

    [调试]：重试次数改为 30 次
    超时时间为 60 秒，重试 30 次，github-action 最长运行时间大约为
     30 分钟

--- flashdns.sh
+++ flashdns.sh
@@ -1,14 +1,15 @@
 # 此脚本用于刷新 github-action 中的dns缓存。
-echo '1——————————'
+echo '1cat '/etc/resolv.conf''
 cat '/etc/resolv.conf'
-echo '2——————————'
+echo '2echo "nameserver 114.114.114.114
+nameserver 8.8.8.8">>'/etc/resolv.conf''
 echo "nameserver 114.114.114.114
 nameserver 8.8.8.8">>'/etc/resolv.conf'
-echo '3——————————'
+echo '3cat '/etc/resolv.conf''
 cat '/etc/resolv.conf'
-echo '4——————————'
+echo '4sudo systemctl restart systemd-resolved'
 sudo systemctl restart systemd-resolved
-echo '5——————————'
+echo '5sudo resolvectl statistics'
 sudo resolvectl statistics
-echo '6——————————'
+echo '6sudo resolvectl flush-caches'
 sudo resolvectl flush-caches
