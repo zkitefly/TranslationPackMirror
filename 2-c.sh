a0f0e127deaec1d101b4e25807dea91c69ff4b4f
zkitefly <2573874409@qq.com>
2023-08-04 06:49:24 +0000

    update 2T

--- 2-c.sh
+++ 2-c.sh
@@ -20,25 +20,28 @@ export "MD5NAME3=1.18.md5$MD5NAME3"
 export "TNAME4=Minecraft-Mod-Language-Modpack-1-18-Fabric.zip$TNAME4"
 export "sNAME4=Minecraft-Mod-Language-Package-1.18-fabric-$sNAME4"
 export "MD5NAME4=1.18-fabric.md5$MD5NAME4"
+# Minecraft-Mod-Language-Package-1.19-XXXXXX
+export "TNAME5=Minecraft-Mod-Language-Modpack-1-19.zip$TNAME5"
+export "sNAME5=Minecraft-Mod-Language-Package-1.19-$sNAME5"
+export "MD5NAME5=1.19.md5$MD5NAME5"
 
 cd './files-2444'
 echo "files-2444:"
 ls
 echo "==========="
 
-NAME0=$(find . -name "$sNAME0*.zip")
-NAME2=$(find . -name "$sNAME2*.zip") 
+NAME2=$(find . -name "$sNAME2*.zip")
 NAME4=$(find . -name "$sNAME4*.zip") 
 
-mv "$NAME0" ../
 mv "$NAME2" ../
 mv "$NAME4" ../
 
+NAME0=$(find . -name "$sNAME0*.zip")
 NAME1=$(find . -name "$sNAME1*.zip")
 NAME3=$(find . -name "$sNAME3*.zip")
+NAME5=$(find . -name "$sNAME5*.zip")
 
 cd ../
-mv "$NAME0" ./files-2444
 mv "$NAME2" ./files-2444
 mv "$NAME4" ./files-2444
 cd ./files-2444
@@ -46,9 +49,6 @@ cd ./files-2444
 cd ../
 
 cd './files-2444-T'
-
-mv 'Minecraft-Mod-Language-Modpack-1-10-2.zip' ../
-mv '1.10.2.md5' ../
 
 rm *.zip
 rm *.md5
@@ -66,24 +66,28 @@ md5sum "${NAME1}" > "$MD5NAME1"
 md5sum "${NAME2}" > "$MD5NAME2"
 md5sum "${NAME3}" > "$MD5NAME3"
 md5sum "${NAME4}" > "$MD5NAME4"
+md5sum "${NAME5}" > "$MD5NAME5"
 
 cut "$MD5NAME0" -c1-32 | tr a-z A-Z>"tmp0.txt"
 cut "$MD5NAME1" -c1-32 | tr a-z A-Z>"tmp1.txt"
 cut "$MD5NAME2" -c1-32 | tr a-z A-Z>"tmp2.txt"
 cut "$MD5NAME3" -c1-32 | tr a-z A-Z>"tmp3.txt"
 cut "$MD5NAME4" -c1-32 | tr a-z A-Z>"tmp4.txt"
+cut "$MD5NAME5" -c1-32 | tr a-z A-Z>"tmp5.txt"
 
 cat "tmp0.txt" | tr '\n' ' '>"$MD5NAME0"
 cat "tmp1.txt" | tr '\n' ' '>"$MD5NAME1"
 cat "tmp2.txt" | tr '\n' ' '>"$MD5NAME2"
 cat "tmp3.txt" | tr '\n' ' '>"$MD5NAME3"
 cat "tmp4.txt" | tr '\n' ' '>"$MD5NAME4"
+cat "tmp5.txt" | tr '\n' ' '>"$MD5NAME5"
 
 sed -i 's\ \\g' "$MD5NAME0"
 sed -i 's\ \\g' "$MD5NAME1"
 sed -i 's\ \\g' "$MD5NAME2"
 sed -i 's\ \\g' "$MD5NAME3"
 sed -i 's\ \\g' "$MD5NAME4"
+sed -i 's\ \\g' "$MD5NAME5"
 
 rm *.txt
 mv *.md5 ../
@@ -105,39 +109,41 @@ cp "${NAME1}" "$TNAME1"
 cp "${NAME2}" "$TNAME2"
 cp "${NAME3}" "$TNAME3"
 cp "${NAME4}" "$TNAME4"
+cp "${NAME5}" "$TNAME5"
 
 mv "$TNAME0" ../
 mv "$TNAME1" ../
 mv "$TNAME2" ../
 mv "$TNAME3" ../
 mv "$TNAME4" ../
+mv "$TNAME5" ../
 
 cd ../
 mv *.zip files-2444-T
 
 ##########################################################################
 
-####################################1.19##################################
+####################################1.10##################################
 cd './files'
 
-cp 'Minecraft-Mod-Language-Modpack-1-19.zip' 'Minecraft-Mod-Language-Modpack-1-191.zip'
-cp '1.19.md5' '1.191.md5'
+cp 'Minecraft-Mod-Language-Modpack-1-10-2.zip' 'Minecraft-Mod-Language-Modpack-1-10-21.zip'
+cp '1.10.2.md5' '1.10.21.md5'
 
-mv 'Minecraft-Mod-Language-Modpack-1-191.zip' ../
-mv '1.191.md5' ../
+mv 'Minecraft-Mod-Language-Modpack-1-10-21.zip' ../
+mv '1.10.21.md5' ../
 
 cd ../
 
-mv 'Minecraft-Mod-Language-Modpack-1-191.zip' 'files-2444-T'
-mv '1.191.md5' 'files-2444-T'
+mv 'Minecraft-Mod-Language-Modpack-1-10-21.zip' 'files-2444-T'
+mv '1.10.21.md5' 'files-2444-T'
 
 cd './files-2444-T'
 
-cp 'Minecraft-Mod-Language-Modpack-1-191.zip' 'Minecraft-Mod-Language-Modpack-1-19.zip'
-cp '1.191.md5' '1.19.md5'
+cp 'Minecraft-Mod-Language-Modpack-1-10-21.zip' 'Minecraft-Mod-Language-Modpack-1-10-2.zip'
+cp '1.10.21.md5' '1.10.2.md5'
 
-rm 'Minecraft-Mod-Language-Modpack-1-191.zip'
-rm '1.191.md5'
+rm 'Minecraft-Mod-Language-Modpack-1-10-21.zip'
+rm '1.101.md5'
 
 ##########################################################################
 echo "files-2444(NOW):"
